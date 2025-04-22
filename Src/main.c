7bb753db1fb37634df511cce92d7b85a4d5d59dd
yuhhua-hz <ee.hzyuh@gmail.com>
2025-04-22 21:57:30 +0200

    Solucionar LF

--- Src/main.c
+++ Src/main.c
@@ -15,25 +15,24 @@
 
 int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;   // Parar el watchdog
-	PM5CTL0 &= ~LOCKLPM5;       // Desbloquear los puertos
+    WDTCTL = WDTPW | WDTHOLD;   // Parar el watchdog
+    PM5CTL0 &= ~LOCKLPM5;       // Desbloquear los puertos
 
-	// Inicializar reloj, System Timer, LCD, teclado y LEDs
-	csIniLf();
+    // Inicializar reloj, System Timer, LCD, teclado y LEDs
+    csIniLf();
     stIni(periodo);
     lcdIni();
     kbIni();
     ledIni1();
     ledIni2();
 
-	while(1) {
-		tareaReloj();    /**< Actualiza y muestra la hora */
-		tareaTeclado();  /**< Gestiona la entrada del teclado */
-		tareaBateria();  /**< Actualiza la animación de la batería */
-		tareaPuntos();   /**< Controla el parpadeo de los puntos separadores */
-		tareaLed();      /**< Gestiona el estado de los LEDs */
-		LPM3;            /**< Modo de bajo consumo hasta próxima interrupción */
-	}
+    while(1) {
+        tareaReloj();    /**< Actualiza y muestra la hora */
+        tareaTeclado();  /**< Gestiona la entrada del teclado */
+        tareaBateria();  /**< Actualiza la animación de la batería */
+        tareaPuntos();   /**< Controla el parpadeo de los puntos separadores */
+        tareaLed();      /**< Gestiona el estado de los LEDs */
+        LPM3;            /**< Modo de bajo consumo hasta próxima interrupción */
+    }
 
 }
-
